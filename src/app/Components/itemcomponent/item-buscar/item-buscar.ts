f6d7c02b699247d1c746b38a1e73e2cdefb12d94
Oskar-16 <oskarflavioguerreropreciado@gmail.com>
2026-06-14 22:11:26 -0500

    feat: agregar components de item

---
+++ src/app/Components/itemcomponent/item-buscar/item-buscar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-buscar',
+  imports: [],
+  templateUrl: './item-buscar.html',
+  styleUrl: './item-buscar.css',
+})
+export class ItemBuscar {}
