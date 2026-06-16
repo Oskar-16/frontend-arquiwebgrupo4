0c07f56a98bd13db0f280933cda91cf06b370dca
Francesko Montesinos <87133076+Francesko217@users.noreply.github.com>
2026-06-16 10:54:30 -0500

    fix(itemservice): alinear rutas con ItemController del backend
    - base_url ahora /items (antes terminaba en / y generaba doble slash //)
    - list:    GET    /items        (antes /lista)
    - insert:  POST   /items        (antes /nuevo)
    - listId:  GET    /items/{id}
    - eliminar:DELETE /items/{id}
    - update:  PUT    /items/{id}   (antes /actualiza; ahora recibe id)

--- src/app/Services/itemservice.ts
+++ src/app/Services/itemservice.ts
@@ -2,32 +2,32 @@ import { Injectable } from "@angular/core";
 import { enviroment } from "../../Enviroments/enviroments";
 import { HttpClient } from '@angular/common/http';
 import { Item } from "../Models/item";
-const base_url=enviroment.base;
+
 @Injectable({
-    providedIn:'root',
+    providedIn: 'root',
 })
-export class Itemservice{
-    private url=`${base_url}/`;
-    constructor(private http:HttpClient){}
-    
-  list() {
-    return this.http.get<Item[]>(`${this.url}/lista`);
-  }
+export class Itemservice {
+    private url = `${enviroment.base}/items`;
 
-  insert(i:Item) {
-    return this.http.post(`${this.url}/nuevo`, i);
-  }
+    constructor(private http: HttpClient) {}
 
-  eliminar(id: number) {
-    return this.http.delete(`${this.url}/${id}`, { responseType: 'text' })
-  }
+    list() {
+        return this.http.get<Item[]>(this.url);
+    }
 
-  listId(id: number) {
-    return this.http.get<Item>(`${this.url}/${id}`)
-  }
+    insert(i: Item) {
+        return this.http.post(this.url, i);
+    }
 
+    eliminar(id: number) {
+        return this.http.delete(`${this.url}/${id}`, { responseType: 'text' });
+    }
 
-  update(i:Item) {
-    return this.http.put(`${this.url}/actualiza`, i, { responseType: 'text' })
-  }
+    listId(id: number) {
+        return this.http.get<Item>(`${this.url}/${id}`);
+    }
+
+    update(id: number, i: Item) {
+        return this.http.put(`${this.url}/${id}`, i, { responseType: 'text' });
+    }
 }
